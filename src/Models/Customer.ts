8b963615cb6d14e140eb98ff30a2c07465011904
ASHOK KUMAR <ashokkumarlm10@gmail.com>
2023-04-05 15:58:29 +0530

    models

---
+++ src/Models/Customer.ts
@@ -0,0 +1,16 @@
+export class Customer
+{
+    CustomerId:string;
+    EmailId:string;
+    Firstname:string;
+    Lastname:string;
+    Address:string;
+    Phone:string;
+    PasswordHash:string;
+    PasswordSalt:string;
+    PanNumber:string;
+    DOB:Date;
+    CustomerCreationStatusId:number;
+
+
+}
