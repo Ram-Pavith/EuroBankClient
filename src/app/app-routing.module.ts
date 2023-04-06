e6bdba2815fe0e959ae9d18a4b95b947c497c226
ASHOK KUMAR <ashokkumarlm10@gmail.com>
2023-04-06 16:35:36 +0530

    customer module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './customer/Components/home/home.component';
+import { GetCustomerAccountsComponent } from './customer/Components/get-customer-accounts/get-customer-accounts.component';
+import { GetAccountComponent } from './customer/Components/get-account/get-account.component';
 
 const routes: Routes =[
  { 
    path:"home",component:HomeComponent
+  },
+  {
+    path:"getcustomer",component:GetCustomerAccountsComponent
+  },
+  {
+    path:"getaccount",component:GetAccountComponent
   }
 
  ];
