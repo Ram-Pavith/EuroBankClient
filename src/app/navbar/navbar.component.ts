81976514e726fd425b7eda48822a110e5b83f20d
ASHOK KUMAR <ashokkumarlm10@gmail.com>
2023-04-08 18:57:39 +0530

    final customer push

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent 
+{
+  constructor(private route:Router)
+  {
+
+  }
+  Logout()
+  {
+    localStorage.clear();
+    this.route.navigateByUrl('CustomerLogin');
+  }
 
 }
