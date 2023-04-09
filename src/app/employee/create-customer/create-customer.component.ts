4b03e992c0889d84f70b797467396c7cd1dbb1c7
parthe11 <89572506+parthe11@users.noreply.github.com>
2023-04-09 13:33:29 +0530

    Angular Testing

--- src/app/employee/create-customer/create-customer.component.ts
+++ src/app/employee/create-customer/create-customer.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { EmployeeservService } from '../Services/employeeserv.service';
 import { Customer } from 'src/Models/Customer';
 import { CustomerRegister } from 'src/Models/CustomerRegister';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-create-customer',
@@ -28,6 +29,7 @@ export class CreateCustomerComponent {
   createCustomer(){
     //console.log(this.customer.dob.toISOString());
     //this.customer.dob = this.Dob.toISOString();
+    //this.customer.dob = formatDate(this.Dob,'mm/dd/yyyy','en-US')
     this.empservice.createCustomer(this.customer).subscribe(data =>{
       console.log(data);
       this.msg = "Success!!"
